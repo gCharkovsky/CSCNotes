0db31bc94587b3acadda0cb7b39d0bc60479c461
gCharkovsky <g.charkovsky@gmail.com>
2019-10-11 20:04:34 +0300

    hw2: theme toggle

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 var time = (Date.now()%(1000*3600*24))/3600000 + 3;
 if(time<8 || time>20)
     document.body.className = "dark-theme";
+
+
+function toggle_theme() {
+    if(document.body.className === "dark-theme"){
+        document.body.className = "";
+    } else {
+        document.body.className = "dark-theme";
+    }
+}
+
+document.getElementById("theme_toggler").addEventListener("click", toggle_theme);

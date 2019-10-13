32ce7a287ecb54b863dd3887715030518fd72938
gCharkovsky <g.charkovsky@gmail.com>
2019-10-14 01:40:12 +0300

    hw2: dark theme, search toggle

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-var time = (Date.now()%(1000*3600*24))/3600000 + 3;
-if(time<8 || time>20)
-    document.body.className = "dark-theme";
+function set_theme(){
+    if(document.cookie === "dark=true")
+        document.body.className = "dark-theme";
+    else if(document.cookie === "dark=false"){
 
+    } else{
+        let time = (Date.now() % (1000 * 3600 * 24)) / 3600000 + 3;
+        if (time < 8 || time > 20)
+            document.body.className = "dark-theme";
+    }
+
+
+
+}
+
+set_theme();
+let search = document.getElementsByClassName("header-menu__search")[0];
 
 function toggle_theme() {
-    if(document.body.className === "dark-theme"){
+    if (document.body.className === "dark-theme") {
         document.body.className = "";
+        document.cookie = "dark=false;"
     } else {
         document.body.className = "dark-theme";
+        document.cookie = "dark=true;"
     }
 }
 
-document.getElementById("theme_toggler").addEventListener("click", toggle_theme);
+function show_search() {
+    search.classList.remove("header-menu__search_collapsed");
+}
+
+function hide_search() {
+    if(search.children[1].value === "")
+        search.classList.add("header-menu__search_collapsed");
+}
